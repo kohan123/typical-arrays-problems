0f2f1f5520d5581319ab2cc27fb93787e4761e9a
Danil Kohan <Sportodavka@gmail.com>
2020-10-02 20:50:34 +0300

    feat: create three functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+  let mini = 0;
+  if (array && array.length > 0) {
+    mini = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] < mini) {
+        mini = array[i];
+      }
+    }
+  }
+  return mini;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let max = 0;
+  if (array && array.length > 0) {
+   max = array[0];
+  for (let i = 1; i < array.length; i++) {
+  if (array[i] > max) {
+    max = array[i];
+  }
+}
+  }
+
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  let avg = 0;
+  if (array && array.length > 0) {
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    avg = sum / array.length;
+  }
+  return avg;
 }
